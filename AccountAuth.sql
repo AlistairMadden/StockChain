e270150407016178db0888708642b47b006d29d8
DESKTOP-VB77342\Alistair <alistair.john.madden@gmail.com>
2017-01-09 19:39:36 +0000

    Fix error in SQL statement

--- AccountAuth.sql
+++ AccountAuth.sql
@@ -1,5 +1,6 @@
 CREATE table accountAuth(
 	Account_ID INT UNSIGNED NOT NULL auto_increment,
-    UNIQUE username VARCHAR(50) not null,
+    username VARCHAR(50) not null,
     password CHAR(60) not null,
+    unique(username),
     primary key(Account_ID))

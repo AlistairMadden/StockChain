b5339c4762f332114e8a7067afe1e44145ac82fd
Alistair Madden <Alistair.John.Madden@gmail.com>
2017-03-14 10:00:11 +0000

    Fix stored procedure issues related to changing months

--- GetAccountStockBalance.sql
+++ GetAccountStockBalance.sql
@@ -1,50 +1,53 @@
 drop procedure get_account_stock_balance;
 
+## Possible code duplication - Stock balance can be derived from the balance in £s (or visa versa)
+
 DELIMITER $$
-create procedure get_account_stock_balance(IN inputUsername VARCHAR(50)) 
+create procedure get_account_stock_balance(IN inputUsername VARCHAR(50))
 BEGIN
 
-# Stock held by account * £ per stock
-select (Closing_Balance + Account_Change) as balance
-from
-	# Previous statement stock determination (if exists)
-	(select ifnull(
-		(select Closing_Balance * Quote
-		FROM stockchain.accountstatement
-		join stockchain.nav_value
-		on date(stockchain.accountstatement.Statement_Date) = stockchain.nav_value.Quote_Date
-		WHERE Statement_Date = 
-			(select MAX(stockchain.accountstatement.Statement_Date) 
-			from stockchain.accountstatement 
-			where Account_ID = 
-				(select Account_ID 
-				from stockchain.accountauth 
-				where username = inputUsername
-				)
-			)
-		limit 1
-        ), 0) AS Closing_Balance
-	) as MostRecentBalance
+# Stock held by account
+SELECT (Closing_Balance + IFNULL(Account_Change, 0)) as balance
+FROM
+	# Previous statement stock determination
+    (SELECT IFNULL(Closing_Balance, 0) AS Closing_Balance
+     FROM
+        (SELECT Closing_Balance * Quote AS Closing_Balance
+		 FROM stockchain.accountstatement
+		 JOIN stockchain.nav_value 
+         ON DATE(stockchain.accountstatement.Statement_Date) = stockchain.nav_value.Quote_Date
+		 WHERE Statement_Date = (SELECT MAX(stockchain.accountstatement.Statement_Date)
+		 						 FROM stockchain.accountstatement
+								 WHERE Account_ID = (SELECT Account_ID
+													 FROM stockchain.accountauth
+													 WHERE username = inputUsername)
+								)
+		 AND Account_ID = (SELECT Account_ID
+						   FROM stockchain.accountauth
+						   WHERE username = inputUsername)
+		 ) AS another_table
+	) AS previous_statement
     
-	cross join
+LEFT JOIN
 
-	# Recent transactions stock determination
-	(Select SUM(CASE Transactions.Transaction_Code WHEN "C" THEN Transactions.Transaction_Amount * Transactions.Quote WHEN "D" 
-	THEN -Transactions.Transaction_Amount * Transactions.Quote END) AS Account_Change 
-    FROM
-		(SELECT Transaction_Amount, Transaction_Code, Quote
-		FROM stockchain.accounttransaction
-        join stockchain.nav_value
-        on date(if(time(Transaction_DateTime) < "16:40:00", date_sub(Transaction_DateTime, interval 1 day), Transaction_DateTime)) = nav_value.Quote_Date
-		WHERE Transaction_DateTime between DATE_FORMAT(NOW() ,'%Y-%m-01') AND NOW() 
-        AND Account_ID = 
-			(select Account_ID 
-            from accountauth 
-            where username = inputUsername
-            )
+	# Stock contribution of transactions from first of the month to current time.
+    (SELECT SUM((CASE Transactions.Transaction_Code
+			 WHEN 'C' THEN Transactions.Transaction_Amount * Transactions.Quote
+			 WHEN 'D' THEN - Transactions.Transaction_Amount * Transactions.Quote END)) AS Account_Change
+	 FROM
+        (SELECT Account_ID, Transaction_Amount, Transaction_Code, Quote
+		 FROM stockchain.accounttransaction
+		 JOIN stockchain.nav_value 
+         ON DATE(IF(TIME(Transaction_DateTime) < '16:40:00', DATE_SUB(Transaction_DateTime, INTERVAL 1 DAY), Transaction_DateTime)) = nav_value.Quote_Date
+		 WHERE Transaction_DateTime BETWEEN DATE_FORMAT(NOW(), '%Y-%m-01') AND NOW()
+		 AND Account_ID = (SELECT Account_ID
+						   FROM accountauth
+						   WHERE username = inputUsername)
 		) AS Transactions
-	) AS TransactionsTotal;
+	) AS table2 
+ON Account_Change;
+
 END $$
 DELIMITER ;
 
-call get_account_stock_balance("alistair.john.madden@gmail.com")
+call get_account_stock_balance("alistair.madden@me.com");
--- UpdateAccountStatement.sql
+++ UpdateAccountStatement.sql
@@ -1,22 +1,34 @@
-drop procedure updateaccountstatements;
+drop procedure update_account_statement;
 
 DELIMITER $$
-create procedure updateaccountstatements(IN NewStatementDate date, PreviousStatementDate date) 
+create procedure update_account_statement() 
 BEGIN
-insert into stockchain.accountstatement 
-select CarriedForwardBalance.Account_ID, CarriedForwardBalance.Statement_Date, CarriedForwardBalance.Closing_Balance + IFNULL(TransactionsTotal.Account_Change, 0) AS Closing_Balance 
-from (select stockchain.accountauth.Account_ID, NewStatementDate AS Statement_Date, IFNULL(MostRecentStatements.Closing_Balance, 0) AS Closing_Balance from stockchain.accountauth
-LEFT JOIN
-(SELECT * FROM stockchain.accountstatement
-WHERE Statement_Date = PreviousStatementDate) AS MostRecentStatements
-ON stockchain.accountauth.Account_ID = MostRecentStatements.Account_ID) AS CarriedForwardBalance
+insert into stockchain.accountstatement
+select CarriedForwardBalance.Account_ID, CarriedForwardBalance.Statement_Date, CarriedForwardBalance.Closing_Balance + IFNULL(Transactions.Account_Change, 0) AS Closing_Balance 
+from 
+	(select stockchain.accountauth.Account_ID,  DATE_FORMAT(now(),'%Y-%m-01') AS Statement_Date, IFNULL(MostRecentStatements.Closing_Balance, 0) AS Closing_Balance 
+	from stockchain.accountauth
+		LEFT JOIN
+		(SELECT * 
+        FROM stockchain.accountstatement
+		WHERE Statement_Date = DATE_FORMAT(date_sub(now(), interval 1 month) ,'%Y-%m-01')) AS MostRecentStatements
+		ON stockchain.accountauth.Account_ID = MostRecentStatements.Account_ID) AS CarriedForwardBalance
 LEFT JOIN 
-(Select Account_ID, SUM(CASE Transactions.Transaction_ID WHEN "C" THEN Transactions.Transaction_Amount WHEN "D" THEN -Transactions.Transaction_Amount END) AS Account_Change FROM
-(SELECT Account_ID, Transaction_Amount, Transaction_ID 
-FROM stockchain.accounttransaction
-WHERE Transaction_DateTime between PreviousStatementDate and NewStatementDate)
-AS Transactions
-group by Transactions.Account_ID) AS TransactionsTotal
-ON CarriedForwardBalance.Account_ID = TransactionsTotal.Account_ID;
+(Select Account_ID, DATE_FORMAT(now(),'%Y-%m-01') as Statement_Date, ((1/Quote) * Account_Change) as Account_Change
+from
+	(Select Account_ID, SUM(CASE Transactions.Transaction_Code WHEN "C" THEN Transactions.Transaction_Amount * Transactions.Quote WHEN "D" 
+		THEN -Transactions.Transaction_Amount * Transactions.Quote END) AS Account_Change 
+		FROM
+			(SELECT Transaction_Amount, Transaction_Code, Quote, Account_ID
+			FROM 
+				stockchain.accounttransaction
+				join stockchain.nav_value
+				on date(if(time(Transaction_DateTime) < "16:40:00", date_sub(Transaction_DateTime, interval 1 day), Transaction_DateTime)) = nav_value.Quote_Date
+				WHERE Transaction_DateTime between DATE_FORMAT(date_sub(now(), interval 1 month) ,'%Y-%m-01') AND DATE_FORMAT(now(),'%Y-%m-01')
+			) AS Transactions 
+		group by Account_ID) as account_change
+	join stockchain.nav_value
+    on date_format(now(), '%Y-%m-01') = nav_value.Quote_Date) as Transactions
+ON CarriedForwardBalance.Account_ID = Transactions.Account_ID;
 END $$
 DELIMITER ;
